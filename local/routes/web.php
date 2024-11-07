13b4ed3d5aba6ed9f69d80e1c8727ff1e22be258
Eloissa <mariaeloissa.andal@tup.edu.ph>
2024-11-07 16:13:10 +0800

    added npm run build set up

--- local/routes/web.php
+++ local/routes/web.php
@@ -43,3 +43,6 @@ Route::middleware(['preventBackHistory'])->group(function() {
 Route::middleware(['redirectIfAuthenticated'])->group(function() {
     Route::get('/home', [HomeController::class, 'index'])->name('home');
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
