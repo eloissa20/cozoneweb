83bdf768c749e9e473306cc0ca2c82c9de53b688
jayannbal <144776229+jayannbal@users.noreply.github.com>
2024-11-10 02:14:11 +0800

    added new css file in assets

---
+++ local/app/Http/Controllers/CoworkerController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class CoworkerController extends Controller
+{
+    // public function viewDashboard()
+    // {
+    //     return view('coworker_side.coworker');
+    // }
+
+    public function viewDashboard()
+    {
+        // Fetch users from the database
+        $users = DB::table('users')->select('id', 'name', 'email', 'created_at')->get();
+
+        // Total users and percentage change calculation
+        $totalUsers = $users->count();
+        $lastMonth = Carbon::now()->subMonth();
+        $usersLastMonth = DB::table('users')
+            ->whereYear('created_at', $lastMonth->year)
+            ->whereMonth('created_at', $lastMonth->month)
+            ->count();
+
+        $monthBeforeLast = Carbon::now()->subMonths(2);
+        $usersMonthBeforeLast = DB::table('users')
+            ->whereYear('created_at', $monthBeforeLast->year)
+            ->whereMonth('created_at', $monthBeforeLast->month)
+            ->count();
+
+        $percentageChange = $usersMonthBeforeLast > 0 
+            ? (($usersLastMonth - $usersMonthBeforeLast) / $usersMonthBeforeLast) * 100 
+            : ($usersLastMonth > 0 ? 100 : 0);
+
+        $percentageChange = round($percentageChange, 1);
+
+        $data = [
+            'total_users' => $totalUsers,
+            'percentage_change' => $percentageChange,
+            'users' => $users,
+        ];
+
+        return view('coworker_side.coworker', ['data' => $data]);
+    }
+
+    public function viewListSpace()
+    {
+        return view('coworker_side.listSpace');
+    }
+    public function viewmyCoworkingSpace()
+    {
+        return view('coworker_side.myCoworkingSpace');
+    }
+    public function submitListSpace(Request $request)
+    {
+
+        // dd($request->all());
+    $request->validate([
+        'headerImage' => 'nullable|mimes:png,jpg,jpeg,webp',
+    ], [
+        'headerImage.mimes' => 'The header image must be a file of type: png, jpg, jpeg, webp.',
+    ]);
+
+
+    if ($request->hasFile('headerImage')) {
+        $file = $request->file('headerImage');
+        $extension = $file->getClientOriginalExtension();
+        $filename = time() . '.' . $extension;
+        $path = 'uploads/header/';
+        $file->move($path, $filename);
+    } else {
+        $filename = null;
+    }
+
+    $data = [
+        'role' => $request->input('role'),
+        'coworking_space_name' => $request->input('coworkingSpaceName'),
+        'coworking_space_address' => $request->input('coworkingSpaceAddress'),
+        'space_name' => $request->input('spaceName'),
+        'type_of_space' => $request->input('typeOfSpace'),
+        'description' => $request->input('description'),
+        'opening_date' => $request->input('openingDate'),
+        'available_days' => $request->input('availableDays'),
+        'operating_hours' => $request->input('operatingHours'),
+        'email' => $request->input('email'),
+        'phone' => $request->input('phone'),
+        'instagram' => $request->input('instagram'),
+        'facebook' => $request->input('facebook'),
+        'contact_no' => $request->input('contactNo'),
+        'basics' => json_encode($request->input('basics')),
+        'seats' => json_encode($request->input('seats')),
+        'equipment' => json_encode($request->input('equipment')),
+        'facilities' => json_encode($request->input('facilities')),
+        'accessibility' => json_encode($request->input('accessibility')),
+        'perks' => json_encode($request->input('perks')),
+        'location' => $request->input('location'),
+        'telephone' => $request->input('telephone'),
+        'country' => $request->input('country'),
+        'unit' => $request->input('unit'),
+        'postal' => $request->input('postal'),
+        'city' => $request->input('city'),
+        'tables' => $request->input('tables'),
+        'capacity' => $request->input('capacity'),
+        'meeting_rooms' => $request->input('meetingRoomsCount'),
+        'virtual_offices' => $request->input('virtualOffices'),
+        'size' => $request->input('size'),
+        'measurement_unit' => $request->input('measurementUnit'),
+        'header_image' => $path . $filename,
+        'pay_online' => $request->input('payOnline'),
+        'credit_cards' => $request->input('creditCards'),
+        'eWallet' => $request->input('eWallet'),
+        'desk_fields' => json_encode($request->input('desks')),
+        'meeting_fields' => json_encode($request->input('meetingRooms')),
+        'virtual_service' => $request->input('virtualService'),
+        'membership' => $request->input('membership'),
+        'membership_duration' => $request->input('membershipDuration'),
+        'membership_price' => $request->input('membershipPrice'),
+        'short_term' => $request->input('shortTerm'),
+        'short_term_details' => $request->input('shortTermDetails'),
+        'free_pass' => $request->input('freePass'),
+        'free_pass_details' => $request->input('freePassDetails'),
+    ];
+
+    DB::table('list_space_tbl')->insert($data);
+
+    return response()->json(['message' => 'Data inserted successfully.']);
+}
+
+}
