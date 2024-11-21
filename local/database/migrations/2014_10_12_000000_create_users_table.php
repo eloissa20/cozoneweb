1350503d3d615561acdea14a2d365321cd78b3ec
jayannbal <144776229+jayannbal@users.noreply.github.com>
2024-11-21 23:07:04 +0800

    update migrations

--- local/database/migrations/2014_10_12_000000_create_users_table.php
+++ local/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->date('birthday')->nullable();
             $table->string('gender')->nullable();
             $table->string('address')->nullable();
+            $table->integer('user_type');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- local/database/migrations/2024_11_19_120421_create_transactions_table.php
+++ local/database/migrations/2024_11_19_120421_create_transactions_table.php
@@ -12,6 +12,61 @@ return new class extends Migration {
      */
     public function up(): void
     {
+        Schema::create('list_space_tbl', function (Blueprint $table) {
+            $table->id();
+            $table->string('role')->nullable();
+            $table->string('coworking_space_name')->nullable();
+            $table->string('coworking_space_address')->nullable();
+            $table->string('space_name')->nullable();
+            $table->string('type_of_space')->nullable();
+            $table->string('description')->nullable();
+            $table->date('opening_date')->nullable();
+            $table->string('available_days_from')->nullable();
+            $table->string('available_days_to')->nullable();
+            $table->string('exceptions')->nullable();
+            $table->string('operating_hours_from')->nullable();
+            $table->string('operating_hours_to')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone', 20)->nullable();
+            $table->string('instagram')->nullable();
+            $table->string('facebook')->nullable();
+            $table->string('contact_no', 20)->nullable();
+            $table->string('basics')->nullable();
+            $table->string('seats')->nullable();
+            $table->string('equipment')->nullable();
+            $table->string('facilities')->nullable();
+            $table->string('accessibility')->nullable();
+            $table->string('perks')->nullable();
+            $table->string('location')->nullable();
+            $table->string('telephone', 20)->nullable();
+            $table->string('country')->nullable();
+            $table->string('unit')->nullable();
+            $table->string('postal')->nullable();
+            $table->string('city')->nullable();
+            $table->integer('tables')->nullable();
+            $table->integer('capacity')->nullable();
+            $table->integer('meeting_rooms')->nullable();
+            $table->integer('virtual_offices')->nullable();
+            $table->string('measurement_unit')->nullable();
+            $table->integer('size')->nullable();
+            $table->string('header_image')->nullable();
+            $table->string('additional_images')->nullable();
+            $table->string('pay_online')->nullable();
+            $table->string('credit_cards')->nullable();
+            $table->string('eWallet')->nullable();
+            $table->string('desk_fields')->nullable();
+            $table->string('meeting_fields')->nullable();
+            $table->string('virtual_service')->nullable();
+            $table->string('membership')->nullable();
+            $table->integer('membership_duration')->nullable();
+            $table->integer('membership_price')->nullable();
+            $table->string('short_term')->nullable();
+            $table->string('free_pass')->nullable();
+            $table->string('short_term_details')->nullable();
+            $table->string('free_pass_details')->nullable();
+            $table->timestamps();
+        });
+
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(User::class)->constrained()->onDelete('cascade');
@@ -42,5 +97,6 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::dropIfExists('transactions');
+        Schema::dropIfExists('list_space_tbl');
     }
 };
