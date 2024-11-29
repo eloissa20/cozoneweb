2d92d55e5deba92210d565e025c81288a200ab2d
acpr-2023 <144645620+acpr-2023@users.noreply.github.com>
2024-11-29 15:17:23 +0800

    updated

--- local/app/Http/Controllers/AdminController.php
+++ local/app/Http/Controllers/AdminController.php
@@ -1,15 +1,13 @@
 <?php
-
 namespace App\Http\Controllers;
 
 namespace App\Http\Controllers;
-
+use App\Models\Transaction;
 use App\Models\Cowork;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\DeactivatedUser;
-use App\Models\Transactions;
 use Illuminate\Support\Facades\Hash;
 use Yajra\DataTables\DataTables;
 
@@ -37,12 +35,12 @@ class AdminController extends Controller
                                 <a href='{$editRoute}' class='btn btn-outline-dark btn-sm me-2'>
                                     <i class='bi bi-pencil-square'></i> Update
                                 </a>
-                                
+
                                 <form action='{$deactivateRoute}' method='POST' onsubmit='return confirm(\"Are you sure you want to deactivate this user?\");' style='display:inline;'>
                                     {$csrf}
                                     <button type='submit' class='btn btn-outline-dark btn-sm me-2'><i class='bi bi-archive'></i> Deactivate</button>
                                 </form>
-                                
+
                                 <button class='btn btn-outline-dark btn-sm me-2' onclick='viewUserDetails(\"{$row->id}\")'><i class='bi bi-eye'></i> View</button>
                             </div>";
                     return $str;
@@ -81,7 +79,7 @@ class AdminController extends Controller
         return redirect()->route('users')->with('success', 'User saved successfully!');
     }
 
-    
+
     public function editUser($id) {
         $user = User::findOrFail($id);
 
@@ -170,7 +168,7 @@ class AdminController extends Controller
                                     {$csrf}
                                     <button type='submit' class='btn btn-outline-dark btn-sm me-2'><i class='bi bi-arrow-repeat'></i> Activate</button>
                                 </form>
-                                <form action='{$deleteRoute}' method='POST' onsubmit='return confirm(\"Are you sure you want to permanently delete this user?\");' 
+                                <form action='{$deleteRoute}' method='POST' onsubmit='return confirm(\"Are you sure you want to permanently delete this user?\");'
                                 style='display:inline;'>
                                     {$csrf}
                                     <input type='hidden' name='_method' value='DELETE'>
@@ -246,11 +244,11 @@ class AdminController extends Controller
     public function viewUserDetails($id)
     {
         $userDetails = DB::table('users')->where('id', $id)->first();
-    
+
         if (!$userDetails) {
             return response()->json(['error' => 'Space not found.'], 404);
         }
-    
+
         return response()->json($userDetails);
     }
 
@@ -288,7 +286,7 @@ class AdminController extends Controller
 
     public function viewTransactions(Request $request) {
         if ($request->ajax()) {
-            $transactions = Transactions::with('cowork')
+            $transactions = Transaction::with('cowork')
                 ->select('*')
                 ->get();
 
@@ -309,12 +307,12 @@ class AdminController extends Controller
 
     public function viewTransactionDetails($id)
     {
-        $transactionDetails = Transactions::with('cowork')->where('id', $id)->first();
-    
+        $transactionDetails = Transaction::with('cowork')->where('id', $id)->first();
+
         if (!$transactionDetails) {
             return response()->json(['error' => 'Transaction not found.'], 404);
         }
-    
+
         return response()->json($transactionDetails);
     }
 }
--- local/database/migrations/2024_11_14_160544_add_notification_table.php
+++ local/database/migrations/2024_11_14_160544_add_notification_table.php
@@ -1,9 +1,11 @@
 <?php
+
 use App\Models\Transaction;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
 return new class extends Migration {
     /**
      * Run the migrations.
@@ -24,6 +26,7 @@ return new class extends Migration {
             $table->timestamps();
         });
     }
+
     /**
      * Reverse the migrations.
      */
