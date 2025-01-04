a2c12ad48d44f9558ffcc3402d00ae42e6e0c7a8
Eloissa <mariaeloissa.andal@tup.edu.ph>
2025-01-05 01:26:14 +0800

    fixing

--- local/app/Http/Middleware/UserAuth.php
+++ local/app/Http/Middleware/UserAuth.php
@@ -2,27 +2,24 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Closure;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class TrustProxies extends Middleware
+class UserAuth
 {
     /**
-     * The trusted proxies for this application.
+     * Handle an incoming request.
      *
-     * @var array<int, string>|string|null
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    protected $proxies;
+    public function handle(Request $request, Closure $next, $userType)
+    {
+        if(auth()->user()->user_type == $userType){
+            return $next($request);
+        }
 
-    /**
-     * The headers that should be used to detect proxies.
-     *
-     * @var int
-     */
-    protected $headers =
-        Request::HEADER_X_FORWARDED_FOR |
-        Request::HEADER_X_FORWARDED_HOST |
-        Request::HEADER_X_FORWARDED_PORT |
-        Request::HEADER_X_FORWARDED_PROTO |
-        Request::HEADER_X_FORWARDED_AWS_ELB;
+        return redirect()->back();
+
+    }
 }
