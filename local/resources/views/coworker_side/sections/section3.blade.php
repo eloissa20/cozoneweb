42a7d574939a2660cac42fddb2d6157db93a20b6
Eloissa <mariaeloissa.andal@tup.edu.ph>
2025-01-08 00:59:52 +0800

    trying

---
+++ local/resources/views/coworker_side/sections/section3.blade.php
@@ -0,0 +1,232 @@
+<div class="form-section" id="step3">
+    <h2>WHAT YOUR SPACE OFFERS</h2>
+    <hr class="separator-line" />
+    <form id="section3">
+        <div class="row">
+            <!-- Left Column -->
+            <div class="col-md-6">
+                <div class="section-box">
+                    <!-- Basics -->
+                    <div class="basic-container">
+                        <p class="section-title">Basics</p>
+                        <hr class="separator-line" />
+                        <div class="row">
+                            <div class="col-auto form-check">
+                                <input class="form-check-input" type="checkbox" name="basics" value="Wifi" id="wifi" />
+                                <label class="form-check-label" for="wifi">Wifi</label>
+                            </div>
+                            <div class="col-auto form-check">
+                                <input class="form-check-input" type="checkbox" name="basics" value="Air Conditioned" id="airConditioned" />
+                                <label class="form-check-label" for="airConditioned">Air Conditioned</label>
+                            </div>
+                            <div class="col-auto form-check">
+                                <input class="form-check-input" type="checkbox" name="basics" value="Power Backup" id="powerBackup" />
+                                <label class="form-check-label" for="powerBackup">Power Backup</label>
+                            </div>
+                        </div>
+                    </div>                            
+    
+                    <!-- Seats -->
+                    <div class="seat-container">
+                        <p class="section-title mt-0">Seats</p>
+                        <hr class="separator-line" />
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="seats" value="Couches" id="couches" />
+                                    <label class="form-check-label" for="couches">Couches</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="seats" value="Beanbag" id="beanbag" />
+                                    <label class="form-check-label" for="beanbag">Beanbag</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="seats" value="Ergonomic Chairs" id="ergonomicChairs" />
+                                    <label class="form-check-label" for="ergonomicChairs">Ergonomic Chairs</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="seats" value="Counter Height Bar Stools" id="barStools" />
+                                    <label class="form-check-label" for="barStools">Counter Height Bar Stools</label>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="seats" value="Modular Seating" id="modularSeating" />
+                                    <label class="form-check-label" for="modularSeating">Modular Seating</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="seats" value="Lounge Chairs" id="loungeChairs" />
+                                    <label class="form-check-label" for="loungeChairs">Lounge Chairs</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="seats" value="Floor Cushions" id="floorCushions" />
+                                    <label class="form-check-label" for="floorCushions">Floor Cushions</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+    
+                    <!-- Equipment -->
+                    <div class="equipment-container">
+                        <p class="section-title mt-0">Equipment</p>
+                        <hr class="separator-line" />
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="equipment" value="Printer" id="printer" />
+                                    <label class="form-check-label" for="printer">Printer</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="equipment" value="Projector" id="projector" />
+                                    <label class="form-check-label" for="projector">Projector</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="equipment" value="Whiteboards" id="whiteboards" />
+                                    <label class="form-check-label" for="whiteboards">Whiteboards</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="equipment" value="Scanner" id="scanner" />
+                                    <label class="form-check-label" for="scanner">Scanner</label>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="equipment" value="Photocopier" id="photocopier" />
+                                    <label class="form-check-label" for="photocopier">Photocopier</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="equipment" value="Computers" id="computers" />
+                                    <label class="form-check-label" for="computers">Computers</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="equipment" value="Laptops" id="laptops" />
+                                    <label class="form-check-label" for="laptops">Laptops</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="equipment" value="Sockets" id="sockets" />
+                                    <label class="form-check-label" for="sockets">Sockets</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+            <!-- Right Column -->
+            <div class="col-md-6">
+                <div class="section-box">
+                    <!-- Facilities -->
+                    <div class="facilities-container">
+                        <p class="section-title">Facilities</p>
+                        <hr class="separator-line" />
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="facilities" value="Kitchen" id="kitchen" />
+                                    <label class="form-check-label" for="kitchen">Kitchen</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="facilities" value="Personal Lockers" id="personalLockers" />
+                                    <label class="form-check-label" for="personalLockers">Personal Lockers</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="facilities" value="Shower Areas" id="showerAreas" />
+                                    <label class="form-check-label" for="showerAreas">Shower Areas</label>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="facilities" value="Nap Room" id="napRoom" />
+                                    <label class="form-check-label" for="napRoom">Nap Room</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="facilities" value="Lounge Area" id="loungeArea" />
+                                    <label class="form-check-label" for="loungeArea">Lounge Area</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="facilities" value="Quiet Room" id="quietRoom" />
+                                    <label class="form-check-label" for="quietRoom">Quiet Room</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+    
+                    <!-- Accessibility -->
+                    <div class="accessibility-container">
+                        <p class="section-title mt-0">Accessibility</p>
+                        <hr class="separator-line" />
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="accessibility" value="Parking" id="parking" />
+                                    <label class="form-check-label" for="parking">Parking</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="accessibility" value="Restrooms" id="restrooms" />
+                                    <label class="form-check-label" for="restrooms">Restrooms</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="accessibility" value="Accessible Workstations" id="accessibleWorkstations" />
+                                    <label class="form-check-label" for="accessibleWorkstations">Accessible
+                                        Workstations</label>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="accessibility" value="Wide Doorways" id="wideDoorways" />
+                                    <label class="form-check-label" for="wideDoorways">Wide Doorways</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="accessibility" value="Adjustable Lighting" id="adjustableLighting" />
+                                    <label class="form-check-label" for="adjustableLighting">Adjustable Lighting</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+    
+                    <!-- Perks -->
+                    <div class="perks-container">
+                        <p class="section-title mt-0">Perks</p>
+                        <hr class="separator-line" />
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="perks" value="Free Drinking Water" id="freeWater" />
+                                    <label class="form-check-label" for="freeWater">Free Drinking Water</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="perks" value="Free Snacks" id="freeSnacks" />
+                                    <label class="form-check-label" for="freeSnacks">Free Snacks</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="perks" value="Vending Machine" id="vendingMachine" />
+                                    <label class="form-check-label" for="vendingMachine">Vending Machine</label>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="perks" value="Snacks/Drinks Available for Purchase" id="snacksPurchase" />
+                                    <label class="form-check-label" for="snacksPurchase">Snacks/Drinks Available for
+                                        Purchase</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="perks" value="Free Coffee" id="freeCoffee" />
+                                    <label class="form-check-label" for="freeCoffee">Free Coffee</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="perks" value="Free Tea" id="freeTea" />
+                                    <label class="form-check-label" for="freeTea">Free Tea</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex justify-content-end mt-4">
+            <button type="button" class="btn btn-light" id="prevButton3">Previous</button>
+            <button type="button" class="btn btn-dark ms-3" id="nextButton3">Next</button>
+        </div>         
+    </form>
+</div>
